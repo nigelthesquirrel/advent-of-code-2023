e12113ae738662c45474d00d0b5ecb155e23dcb9
nigelthesquirrel <nigelthesquirrel@yahoo.co.uk>
2023-12-02 22:04:27 +0000

    Day2 Part1

---
+++ src/main/kotlin/com/andystubbs/adventofcode2023/day2/Day2Part1.kt
@@ -0,0 +1,43 @@
+package com.andystubbs.adventofcode2023.day2
+
+import com.andystubbs.adventofcode2023.util.readInput
+
+
+class Day2Part1 {
+}
+
+fun main(args: Array<String>) {
+
+    val input = readInput("/day2/part1/input.txt")
+
+    val totalCubes = mapOf("red" to 12, "green" to  13, "blue" to 14)
+
+    val invalidGames = HashSet<Int>();
+
+    input.forEach { it ->
+        val maxCount = HashMap<String, Int>()
+        val gameNumber = it.split(":").first().replace("Game ", "").toInt()
+        val reveals = it.split(":").last().split(";")
+
+        reveals.forEach {
+            val cubes = it.split(", ")
+            cubes.forEach {
+                val count = it.trimStart().split(" ").first().toInt()
+                val color = it.trimStart().split(" ").last()
+                maxCount[color] = Math.max((maxCount[color] ?: 0), count)
+            }
+        }
+        maxCount.entries.forEach() {
+            if (it.value > (totalCubes[it.key] ?: 0)) {
+               invalidGames.add(gameNumber)
+            }
+        }
+    }
+
+    val validGames = HashSet<Int>()
+    for(c in 1..input.size) {
+        if(!invalidGames.contains(c)) validGames.add(c)
+    }
+
+    println(validGames.sum())
+}
